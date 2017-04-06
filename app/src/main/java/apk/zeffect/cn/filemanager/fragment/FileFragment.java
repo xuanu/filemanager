b2e53e8fd17140e73919e77d4b7fa6ca041fb92d
zeffect <625417632@qq.com>
2017-04-06 15:36:11 +0800

    修改打开方式

--- app/src/main/java/apk/zeffect/cn/filemanager/fragment/FileFragment.java
+++ app/src/main/java/apk/zeffect/cn/filemanager/fragment/FileFragment.java
@@ -103,7 +103,7 @@ public class FileFragment extends Fragment {
                 if (tempBean.getFile().isDirectory()) {
                     refreshFiles(tempBean.getFile());
                 } else if (tempBean.getFile().isFile()) {
-                    test(tempBean.getFile());
+                    startActivity(OpenFiles.openFile(tempBean.getFile().getAbsolutePath()));
                 }
             }
         });
@@ -151,68 +151,5 @@ public class FileFragment extends Fragment {
                 .setType(type)
                 .setShowName(TextUtils.isEmpty(name) ? tempFile.getName() : name);
         return tempFileBean;
-    }
-
-
-    private void test(File currentPath) {
-        if (currentPath != null && currentPath.isFile()) {
-            String fileName = currentPath.toString();
-            Intent intent;
-            if (checkEndsWithInStringArray(fileName, getResources().
-                    getStringArray(R.array.fileEndingImage))) {
-                intent = OpenFiles.getImageFileIntent(currentPath);
-                startActivity(intent);
-            } else if (checkEndsWithInStringArray(fileName, getResources().
-                    getStringArray(R.array.fileEndingWebText))) {
-                intent = OpenFiles.getHtmlFileIntent(currentPath);
-                startActivity(intent);
-            } else if (checkEndsWithInStringArray(fileName, getResources().
-                    getStringArray(R.array.fileEndingPackage))) {
-                intent = OpenFiles.getApkFileIntent(currentPath);
-                startActivity(intent);
-
-            } else if (checkEndsWithInStringArray(fileName, getResources().
-                    getStringArray(R.array.fileEndingAudio))) {
-                intent = OpenFiles.getAudioFileIntent(currentPath);
-                startActivity(intent);
-            } else if (checkEndsWithInStringArray(fileName, getResources().
-                    getStringArray(R.array.fileEndingVideo))) {
-                intent = OpenFiles.getVideoFileIntent(currentPath);
-                startActivity(intent);
-            } else if (checkEndsWithInStringArray(fileName, getResources().
-                    getStringArray(R.array.fileEndingText))) {
-                intent = OpenFiles.getTextFileIntent(currentPath);
-                startActivity(intent);
-            } else if (checkEndsWithInStringArray(fileName, getResources().
-                    getStringArray(R.array.fileEndingPdf))) {
-                intent = OpenFiles.getPdfFileIntent(currentPath);
-                startActivity(intent);
-            } else if (checkEndsWithInStringArray(fileName, getResources().
-                    getStringArray(R.array.fileEndingWord))) {
-                intent = OpenFiles.getWordFileIntent(currentPath);
-                startActivity(intent);
-            } else if (checkEndsWithInStringArray(fileName, getResources().
-                    getStringArray(R.array.fileEndingExcel))) {
-                intent = OpenFiles.getExcelFileIntent(currentPath);
-                startActivity(intent);
-            } else if (checkEndsWithInStringArray(fileName, getResources().
-                    getStringArray(R.array.fileEndingPPT))) {
-                intent = OpenFiles.getPPTFileIntent(currentPath);
-                startActivity(intent);
-            } else {
-//                showMessage("无法打开，请安装相应的软件！");
-            }
-        } else {
-//            showMessage("对不起，这不是文件！");
-        }
-    }
-
-    private boolean checkEndsWithInStringArray(String checkItsEnd,
-                                               String[] fileEndings) {
-        for (String aEnd : fileEndings) {
-            if (checkItsEnd.endsWith(aEnd))
-                return true;
-        }
-        return false;
     }
 }
